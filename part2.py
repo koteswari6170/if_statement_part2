c5896dd42b39ca4b15102b5930a8d9f5ba7bcfc4
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-04-19 14:26:20 +0530

    Add files via upload

---
+++ part2.py
@@ -0,0 +1,41 @@
+# a=int(input("Enter the value of a:"))    #3
+# b=int(input("Enter the value of b:"))    #4
+# if(a<b):
+#     print(f"{a} < {b} is True")          #3 < 4 is True
+
+# a=int(input("Enter the value of a:"))    #3
+# b=int(input("Enter the value of b:"))    #4
+# if(a<b):
+#     print(f"{a} < {b} is True") 
+#     print("Hey python")
+#     print("Its DOne")
+
+# a=int(input("Enter the value of a:"))    #3
+# b=int(input("Enter the value of b:"))    #4
+# if(a<b):
+#      print(f"{a} < {b} is True") 
+#      print("Hey python")
+#      print("Its DOne")
+# print("Try next time")
+
+# a=int(input("Enter the value of a:"))    #7
+# b=int(input("Enter the value of b:"))    #7
+# if(a<b):
+#     print(f"{a} < {b} is True")
+# if(a>b):
+#     print(f"{a} > {b} is True")
+# if(a<=b):
+#     print(f"{a} <= {b} is True")
+# if(a>=b):
+#     print(f"{a} >= {b} is True")
+# if(a==b):
+#     print(f"{a} == {b} is True")
+# if(a!=b):
+#     print(f"{a} != {b} is True")        #28,30,32 lines executed
+
+
+
+
+
+
+
